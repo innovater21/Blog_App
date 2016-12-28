88f4e90d58072faa915771b54a9450fed1152128
innovater21 <ajha025@gmail.com>
2016-12-29 01:32:41 +0530

    updated the association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161226082449) do
+ActiveRecord::Schema.define(version: 20161228183208) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20161226082449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "article_id"
+    t.integer  "comment_id"
+    t.string   "name"
+    t.integer  "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
